da307fc3f02194d856c1c864a38e90f90310b412
kira <tskira@hotmail.com>
2017-07-14 15:08:19 -0300

    copia ok

--- sistema_eq.s
+++ sistema_eq.s
@@ -13,13 +13,20 @@
 .section .data
 
 titulo: .asciz "\n ### A RUA E NOIS ###\n"
+msg_exibe_matrix: .asciz "\n ### PRINTANDO MATRIX ### \n"
+msg_end_malloc: .asciz "\n ### ENDERECO ALOCADO: %d ### \n"
+msg_copia_matrix: .asciz "\n ### MATRIX COPIADA ### \n"
 pede_dados: .asciz "\nEntre com a equacao do tipo Ax + By + Cz = D\n\tNo formato A B C D\n"
 
 formatoint: .asciz "\n%d"
+formatocopia: .asciz "\n%d -> %d\n"
 pulalinha: .asciz "\n"
 naloc: .int 0
 ptr_matrix: .int 0
+ptr_matrix_aux: .int 0
+matrix_aux: .int 0
 tam_matrix: .int 12
+int_aux: .int 0
 
 .section .text
 .globl main
@@ -39,21 +46,46 @@ main:
 
 	# Alocando o vetor a ser utilizado como matrix
 	# para armazenar os coeficientes do sistema:
-
 	pushl naloc
 	call _aloca_matrix
 	addl $4, %esp
 	movl %eax, ptr_matrix
 
+	# criar matrix auxiliar
+	pushl naloc
+	call _aloca_matrix
+	addl $4, %esp
+	movl %eax, matrix_aux
+	movl $matrix_aux, %eax
+	movl %eax, ptr_matrix_aux #ponteiro para a matrix 
+
 	# Inserir valores na matrix
+	pushl ptr_matrix
  	call _le_dados
+	addl $4, %esp
 
 	# Exibe matrix
-	pushl tam_matrix
+	pushl ptr_matrix
 	call _print_matrix
 	pushl $pulalinha
 	call printf
 	addl $8, %esp
+
+	# copia matrix original na auxiliar
+	pushl tam_matrix
+	pushl ptr_matrix_aux
+	pushl ptr_matrix
+	call _copiar_matrix
+	addl $12, %esp
+
+	#imprimir matrix copiada de zoa
+	pushl ptr_matrix_aux
+	call _print_matrix
+	pushl $pulalinha
+	call printf
+	addl $8, %esp
+
+
 
 	jmp _fim
 
@@ -98,10 +130,10 @@ _aloca_matrix:
 _le_dados:
 	pushl %ebp
 	movl %esp, %ebp
+	movl 8(%ebp), %edi
 	pushl $pede_dados
 	call printf
 	addl $4, %esp
-	movl ptr_matrix, %edi
 	movl tam_matrix, %ecx
 	jmp _coloca_na_matrix
 
@@ -121,18 +153,22 @@ _coloca_na_matrix:
 # Procedimento para imprimir a matrix
 #
 # @param:
-# tamanho da matrix
+# ptr_matrix
 #
 _print_matrix:
 	pushl %ebp
 	movl %esp, %ebp
-	movl 8(%ebp), %ecx
-	movl ptr_matrix, %edi
+	pushl $msg_exibe_matrix
+	call printf
+	addl $4, %esp
+	movl $12, %ecx
+	movl 8(%ebp), %edi
 	jmp _exibe_valores
 
 _exibe_valores:
 	pushl %ecx
-	pushl (%edi)
+	movl (%edi), %eax
+	pushl %eax
 	pushl $formatoint
 	call printf
 	addl $4, %edi
@@ -141,6 +177,39 @@ _exibe_valores:
 	loop _exibe_valores
 	popl %ebp
 	ret
+
+#
+# Funcao para copiar matrix em outra
+#
+# @param:
+# matrixorigem
+# matrixdestino
+# tamanho da matrix
+#
+# @returns
+#
+_copiar_matrix:
+	pushl %ebp
+	movl %esp, %ebp
+	movl 8(%ebp), %esi
+	movl 12(%ebp), %edi
+	movl $12, %ecx
+	jmp _ciclo_copia
+
+_ciclo_copia:
+	pushl %ecx
+	movl (%esi), %eax
+	movl %eax, (%edi)
+	addl $4, %edi
+	addl $4, %esi
+	popl %ecx
+	loop _ciclo_copia
+	pushl $msg_copia_matrix
+	call printf
+	addl $4, %esp
+	popl %ebp
+	ret
+
 
 _fim:
 	pushl $0
